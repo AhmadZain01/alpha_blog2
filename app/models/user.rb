5dfde2f2f6650f912669a484ad2c5defd0601850
AhmadZain01 <ahmad.zain@xprolab.com>
2020-03-24 17:56:17 +0500

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord 
-    
+    validates :username, presence: true, uniqueness: {case_sensitive:false},
+               length: {minimum:10, maximum:30}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i           
+    validates :email, presence: true, uniqueness: {case_sensitive:false},
+                length: {minimum:10, maximum:30},
+                formate: {width: VALID_EMAIL_REGEX }
 end
