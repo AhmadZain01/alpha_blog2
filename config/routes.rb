657f715d5757bef72b32e401cfece365c12c9b50
AhmadZain01 <ahmad.zain@xprolab.com>
2020-03-25 19:09:52 +0500

    signup form and one to many relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-    root 'pages#home', to: 'pages#home'
-
-    get 'pages/about', to: 'pages#about'
-  
-resources :articles 
+    root 'pages#home'
+    get 'about', to: 'pages#about'
+     resources :articles 
+     get '/signup', to: 'users#new'
+     resources :users, except: [:new]
 end
